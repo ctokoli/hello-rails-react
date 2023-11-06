cd7725c38dc7d1480b0c368be83ca9c6b6c17801
ctokoli <matumasie@gmail.com>
2023-11-06 17:03:20 +0000

    Update greetings_controller.rb

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,10 +1,10 @@
 class Api::GreetingsController < ApplicationController
   def index
-    @greeting = Greeting.order("RANDOM()").limit(1).first
+    @greeting = Greeting.order('RANDOM()').limit(1).first
     if @greeting
       render json: @greeting
     else
-      render json: { error: "No greeting found" }, status: 404
+      render json: { error: 'No greeting found' }, status: 404
     end
   end
 end
