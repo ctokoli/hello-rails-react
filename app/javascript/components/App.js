d90b1333717735b1943fac6f8f49d72425eb64d1
ctokoli <matumasie@gmail.com>
2023-11-06 17:03:20 +0000

    Update App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Greetings from "./Greetings";
 
 const App = () => {
     return ( 
-        <div>
-            <Greetings />
-        </div>
+        <Routes>
+            <Route path="/" element={<Greetings />} />
+        </Routes>
      );
 }
  
