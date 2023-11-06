3f910c82f271f14cc957465ba7e49f369235b4a9
ctokoli <matumasie@gmail.com>
2023-11-06 13:31:17 +0000

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Entry point for the build script in your package.json
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 function App() {
   return (<h1>Hello World!</h1>);
