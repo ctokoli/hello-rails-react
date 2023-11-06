0282944e20b29c50c62809747cf2d5c742306530
ctokoli <matumasie@gmail.com>
2023-11-06 15:46:19 +0000

    Add App.js

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Greetings from "./Greetings";
+
+const App = () => {
+    return ( 
+        <div>
+            <Greetings />
+        </div>
+     );
+}
+ 
+export default App;
